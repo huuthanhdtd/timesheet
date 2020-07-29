ce31e6276a931cc62dd1db64614cc30fc0291892
huuthanhdtd <huuthanhdtd1@gmail.com>
2020-07-30 00:49:08 +0900

    first commit

---
+++ readme.md
@@ -0,0 +1,14 @@
+## Khái quát
+Bot cho skype viết bằng google script, google sheet
+
+## Cú pháp cơ bản
+1. Checkin:  @OutOfBox おはよう|hi|hello|morning|出勤
+2. Checkout: @OutOfBox bye|night|お疲れ
+3. Đăng ký nghỉ
+  - Hôm nay: @OutOfBox off / @OutOfBox off today
+  - Ngày mai: @OutOfBox off tomorrow
+  - Ngày khác: @OutOfBox off yyyy/M/D
+4. Hủy đăng ký nghỉ
+  - Hôm nay: @OutOfBox cancel off / @OutOfBox cancel off today
+  - Ngày mai: @OutOfBox cancel off tomorrow
+  - Ngày khác: @OutOfBox cancel off yyyy/M/D
---
+++ settings.js
@@ -0,0 +1,140 @@
+// メッセージ送信
+var Settings = {
+  get: function(key)
+  {
+    var ss = SpreadsheetApp.getActiveSpreadsheet();
+    var sheet = ss.getSheetByName("settings");
+    var lastRow = sheet.getLastRow();
+    var value = sheet.getRange(1, 1, lastRow, 2).getValues().filter(x => x[0]==key)
+    
+    if (value && value.length > 0)
+      return value[0][1];
+  
+    return undefined;
+  },
+  set: function(key, value) {
+    var ss = SpreadsheetApp.getActiveSpreadsheet();
+    var sheet = ss.getSheetByName("settings");
+    var lastRow = sheet.getLastRow();
+    var values = sheet.getRange(1, 1, lastRow, 2).getValues() //.filter(x => x[0]==key)
+    values.forEach(
+      (row, i) => {
+        if (row[0] == key){
+          sheet.getRange(i+1, 2, 1, 1).setValue(value);
+        }
+      }
+    );
+    //return message;
+  },
+  getUser: function(username, datetime) {
+    var ss = SpreadsheetApp.getActiveSpreadsheet();
+    var sheet = ss.getSheetByName("worktimes");
+    var labels = sheet.getRange("A1:Z2").getValues();
+    var users = labels[0].map((e,i)=> { 
+                              if (e==="" || i==0) return undefined;
+                              return {'name':e,'skype':labels[1][i]} 
+                              });
+    
+    return users.filter(f=>f !==undefined && f.name===username)[0];
+    
+  },
+  getUsers: function(action, datetime) {
+    var ss = SpreadsheetApp.getActiveSpreadsheet();
+    var sheet = ss.getSheetByName("worktimes");
+    var labels = sheet.getRange("A1:Z2").getValues();
+    var users = labels[0].map((e,i)=> { 
+                              if (e==="" || i==0) return undefined;
+                              return {'name':e,'skype':labels[1][i]} 
+                              }).filter(e=>e!==undefined);
+    Logger.log(users);
+  //return users;
+    //var header = labels[0].filter((e, i)=>(i-1)%3===0 && e!=="");
+    
+    switch (action)
+    {
+        case 'signined':
+          var firstDate = new Date(sheet.getRange("$A$3").getValue());
+          var r = Math.floor((datetime - firstDate)/(24*3600*1000)) + 3;
+          var signed = sheet.getRange("A"+r+":Z"+r).getValues()[0].map((e, i)=>((i-1)%3===0 && e!=="")?users[Math.floor((i-1)/3)]:undefined).filter(e=>e!=undefined);
+          return signed;
+          break;
+        case 'notsignin':
+          var firstDate = new Date(sheet.getRange("$A$3").getValue());
+          var r = Math.floor((datetime - firstDate)/(24*3600*1000)) + 3;
+          var notsigned = sheet.getRange("A"+r+":Z"+r).getValues()[0].map((e, i)=>((i-1)%3===0 && e==="")?users[Math.floor((i-1)/3)]:undefined).filter(e=>e!=undefined);
+          return notsigned;
+          break;
+        case 'notsignout':
+          var firstDate = new Date(sheet.getRange("$A$3").getValue());
+          var r = Math.floor((datetime - firstDate)/(24*3600*1000)) + 3;
+          var notsigned = sheet.getRange("A"+r+":Z"+r).getValues()[0].map((e, i)=>((i-1)%3===1 && e==="")?users[Math.floor((i-1)/3)]:undefined).filter(e=>e!=undefined);
+          return notsigned;
+          break;
+        default:
+          return users
+          break;
+    }
+    
+  }
+}
+
+//function createTrigger()
+//{
+//   // 毎日8時頃に出勤してるかチェックする
+//    ScriptApp.newTrigger('confirmSignIn')
+//      .timeBased()
+//      .everyDays(1)
+//      .atHour(8)
+//      .create();
+//
+//    // 毎日22時頃に退勤してるかチェックする
+//    ScriptApp.newTrigger('confirmSignOut')
+//      .timeBased()
+//      .everyDays(1)
+//      .atHour(19)
+//      .create();
+
+var Utils = {
+  parseDate: function(str) {
+    str = String(str || "").toLowerCase().replace(/[Ａ-Ｚａ-ｚ０-９]/g, function(s) {
+      return String.fromCharCode(s.charCodeAt(0) - 0xFEE0);
+    });
+    var today = new Date();
+    
+    if(str.match(/(明日|tomorrow|tomorow)/i)) {
+      return new Date(today.getFullYear(), today.getMonth(), today.getDate()+1);
+    }
+
+    if(str.match(/(今日|today)/i)) {
+      return today;
+    }
+
+    if(str.match(/(昨日|yesterday)/i)) {
+      return new Date(today.getFullYear(), today.getMonth(), today.getDate()-1);
+    }
+
+    var reg = /((\d{4})[-\/年]{1}|)(\d{1,2})[-\/月]{1}(\d{1,2})/i;
+    var matches = str.match(reg);
+    if(matches) {
+      var year = parseInt(matches[2], 10);
+      var month = parseInt(matches[3], 10);
+      var day = parseInt(matches[4], 10);
+      if((year == undefined) || year < 1970) {
+        //
+        if((today.getMonth() + 1) >= 11 && month <= 2) {
+          year = today.getFullYear() + 1;
+        }
+        else if((today.getMonth() + 1) <= 2 && month >= 11) {
+          year = today.getFullYear() - 1;
+        }
+        else {
+          year = today.getFullYear();
+        }
+      }
+
+      return new Date(year, month-1, day);
+    }
+
+    return new Date();
+  }, 
+}
